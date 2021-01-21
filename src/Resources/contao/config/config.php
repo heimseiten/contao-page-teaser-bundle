94eeed4fcad3bd8f19ceea8b49118d21aba104bc
Niels <info@heimseiten.de>
2021-01-21 09:25:25 +0100

    remove function from template

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -10,5 +10,86 @@
  * @link https://github.com/heimseiten/contao-page-teaser-bundle
  */
 
-    
+function getImage($page_id, $page_pid, $pages, $size_id, $without_text) {    
+    if (TL_MODE == "FE") {
+        if ($pages) {
+            if ($this->only_sub_pages) {
+                $arrResults = \Database::getInstance()->query("SELECT id, pid, title, pageTitle, description,teaser_headline, teaser_text, pageImage FROM `tl_page` WHERE pid in (". implode(',',$pages) .") AND `type`!='folder' AND `type`!='error_404' AND `hide`!='1' AND `published`='1' AND `id`!='".$page_id."' ORDER BY `sorting`;")->fetchAllAssoc();    
+            } else {
+                $arrResults = \Database::getInstance()->query("SELECT id, title, pageTitle, description,teaser_headline, teaser_text, pageImage FROM `tl_page` WHERE id in (". implode(',',$pages) .") AND `type`!='folder' AND `type`!='error_404' AND `hide`!='1' AND `published`='1' AND `id`!='".$page_id."' ORDER BY `sorting`;")->fetchAllAssoc();    
+            }
+        } else {
+            $arrResults = \Database::getInstance()->query(    "SELECT id, title, pageTitle, description,teaser_headline, teaser_text, pageImage FROM `tl_page` WHERE pid =". $page_id ." AND `type`!='folder' AND `type`!='error_404' AND `hide`!='1' AND `published`='1' ORDER BY `sorting`;")->fetchAllAssoc();    
+            if ($arrResults == NULL){ 
+                $arrResults = \Database::getInstance()->query("SELECT id, pid, title, pageTitle, description,teaser_headline, teaser_text, pageImage FROM `tl_page` WHERE pid =". $page_pid ." AND `type`!='folder' AND `type`!='error_404' AND `hide`!='1' AND `published`='1' AND `id`!='".$page_id."' ORDER BY `sorting`;")->fetchAllAssoc();    
+            }            
+        }
+        $length = count($arrResults);
+        for ($i = 0; $i < $length; $i++) {
+            if ( $arrResults[$i]['teaser_headline'] ) {
+                $link_name = $arrResults[$i]['teaser_headline']; } else {
+                    if ( $arrResults[$i]['pageTitle'] ) { $link_name = $arrResults[$i]['pageTitle']; } else {
+                        if ( $arrResults[$i]['title'] ) { $link_name = $arrResults[$i]['title']; }
+                }
+            }
+            $link_name = strip_tags($link_name);
+            echo '<div class="ce_teaser_wrapper pos_'.$i.' ivp" '.$style.'>';
+                echo '<div class="inside">
+                        <a href="{{link_url::'.$arrResults[$i]['id'].'}}" class="ce_teaser_item" aria-label="' . $link_name . '">'; 
+                    if ($arrResults[$i]['pageImage']) { renderPageImage($arrResults[$i]['pageImage']); } 
+                    else {
+                        $pageId = $arrResults[$i]['id'];
+                        $tl_article = \Database::getInstance()->query("SELECT id, pid, cc_bg_image FROM `tl_article` WHERE pid=".$pageId." ORDER BY `sorting` LIMIT 4;")->fetchAllAssoc();
+                        if ($tl_article[0]['cc_bg_image'] !='') {
+                            echo('{{picture::'.\FilesModel::findByUuid($tl_article[0]['cc_bg_image'])->path.'?size='.$size_id.'}}');
+                        } else {  
+                            if ($tl_article[0]['id']) {
+                                $tl_content = \Database::getInstance()->query("SELECT id, pid, invisible, singleSRC FROM `tl_content` WHERE pid=".$tl_article[0]['id']." AND `invisible`!='1' AND `singleSRC`!='' AND ((`addImage`='1') OR (`type`='image')) ORDER BY `sorting` LIMIT 5;")->fetchAllAssoc();
+                                if ($tl_content[0]['singleSRC']!='') { 
+                                    echo('{{picture::'.\FilesModel::findByUuid($tl_content[0]['singleSRC'])->path.'?size='.$size_id.'}}');
+                                } else {
+                                    if ($tl_article[1]['id']) {
+                                        $tl_content = \Database::getInstance()->query("SELECT id, pid, invisible, singleSRC FROM `tl_content` WHERE pid=".$tl_article[1]['id']." AND `invisible`!='1' AND `singleSRC`!='' AND ((`addImage`='1') OR (`type`='image')) ORDER BY `sorting` LIMIT 5;")->fetchAllAssoc();
+                                        if ($tl_content[1]['singleSRC']!='') { 
+                                            echo('{{picture::'.\FilesModel::findByUuid($tl_content[0]['singleSRC'])->path.'?size='.$size_id.'}}');
+                                        } else {
+                                            if ($tl_article[2]['id']) {
+                                                $tl_content = \Database::getInstance()->query("SELECT id, pid, invisible, singleSRC FROM `tl_content` WHERE pid=".$tl_article[2]['id']." AND `invisible`!='1' AND `singleSRC`!='' AND ((`addImage`='1') OR (`type`='image')) ORDER BY `sorting` LIMIT 5;")->fetchAllAssoc();
+                                                if ($tl_content[2]['singleSRC']!='') { 
+                                                    echo('{{picture::'.\FilesModel::findByUuid($tl_content[0]['singleSRC'])->path.'?size='.$size_id.'}}');
+                                                } else {
+                                                    if ($tl_article[3]['id']) {
+                                                        $tl_content = \Database::getInstance()->query("SELECT id, pid, invisible, singleSRC FROM `tl_content` WHERE pid=".$tl_article[3]['id']." AND `invisible`!='1' AND `singleSRC`!='' AND ((`addImage`='1') OR (`type`='image')) ORDER BY `sorting` LIMIT 5;")->fetchAllAssoc();
+                                                        echo('{{picture::'.\FilesModel::findByUuid($tl_content[0]['singleSRC'])->path.'?size='.$size_id.'}}');
+                                                    }    
+                                                }    
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    echo '<div class="text">';
+                        echo '<h2>'.$link_name.'</h2>';
+                        if (!$without_text) {
+                            if ( $arrResults[$i]['teaser_text'] ) { echo '<p>'.$arrResults[$i]['teaser_text'].'</p>'; } 
+                            else { if ( $arrResults[$i]['description'] ) { echo '<p>'.$arrResults[$i]['description'].'</p>'; } }
+                        }
+                    echo '</div>';
+                echo '</a></div>';
+            echo '</div>';
+        }
+    }
+}
 
+function renderPageImage($pageImage) {
+    $multisrc = deserialize ($pageImage);
+    $objFiles = \FilesModel::findMultipleByUuids ($multisrc);
+    if ($objFiles) {
+        while ( $objFiles->next () ) {
+            $image_path = $objFiles->path;
+            echo('{{picture::' . $image_path . '?size='.$size_id.'}}');
+        }
+    }
+}
