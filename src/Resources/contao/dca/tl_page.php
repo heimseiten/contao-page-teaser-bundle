c52ca135752c5ee771c5fa69954d17284ab8a24d
Niels <info@heimseiten.de>
2021-04-14 20:49:37 +0200

    image height is now stored in layout

--- src/Resources/contao/dca/tl_page.php
+++ src/Resources/contao/dca/tl_page.php
@@ -1,19 +1,5 @@
 <?php
 
-/*
- * This file is part of Teaser.
- * 
- * (c) Niels Hegmans 2020 <info@heimseiten.de>
- * @license GPL-3.0-or-later
- * For the full copyright and license information,
- * please view the LICENSE file that was distributed with this source code.
- * @link https://github.com/heimseiten/contao-page-teaser-bundle
- */
-
-/**
- * page
- */
-
 use Contao\Config;
 use Contao\CoreBundle\DataContainer\PaletteManipulator;
 
@@ -22,18 +8,6 @@ PaletteManipulator::create()
     ->addField('teaser_headline','teaser_legend', PaletteManipulator::POSITION_APPEND)
     ->addField('teaser_text',    'teaser_legend', PaletteManipulator::POSITION_APPEND)
     ->applyToPalette('regular', 'tl_page') 
-;
-PaletteManipulator::create()
-    ->addLegend('teaser_legend_root', 'meta_legend', PaletteManipulator::POSITION_BEFORE, true)
-    ->addField('teaser_image_size_id','teaser_legend_root', PaletteManipulator::POSITION_APPEND)
-    ->addField('teaser_without_text','teaser_legend_root', PaletteManipulator::POSITION_APPEND)
-    ->applyToPalette('rootfallback', 'tl_page') 
-;
-PaletteManipulator::create()
-    ->addLegend('teaser_legend_root', 'meta_legend', PaletteManipulator::POSITION_BEFORE, true)
-    ->addField('teaser_image_size_id','teaser_legend_root', PaletteManipulator::POSITION_APPEND)
-    ->addField('teaser_without_text','teaser_legend_root', PaletteManipulator::POSITION_APPEND)
-    ->applyToPalette('root', 'tl_page') 
 ;
 
 $GLOBALS['TL_DCA']['tl_page']['fields'] += [    
@@ -46,16 +20,5 @@ $GLOBALS['TL_DCA']['tl_page']['fields'] += [
         'label'     => &$GLOBALS['TL_LANG']['tl_page']['teaser_text'],
         'inputType' => 'textarea',
         'sql'       => "text NULL"
-    ],
-    'teaser_image_size_id' => [
-        'inputType'        => 'imageSize',
-        'options'          => \System::getImageSizes(),
-        'reference'        => &$GLOBALS['TL_LANG']['MSC'],
-        'eval'             => array( 'rgxp' => 'digit', 'includeBlankOption' => true, 'tl_class'  => 'clr' ),
-        'sql'              => "varchar(64) NOT NULL default ''"
-    ],
-    'teaser_without_text' => [
-        'inputType'     => 'checkbox', 
-        'sql'           => "char(1) NOT NULL default ''"
     ],
 ];
--- src/Resources/contao/languages/de/default.php
+++ src/Resources/contao/languages/de/default.php
@@ -1,27 +1,14 @@
 <?php
 
-/*
- * This file is part of Teaser.
- * 
- * (c) Niels Hegmans 2020 <info@heimseiten.de>
- * @license GPL-3.0-or-later
- * For the full copyright and license information,
- * please view the LICENSE file that was distributed with this source code.
- * @link https://github.com/heimseiten/contao-page-teaser-bundle
- */
-
-/**
- * Content element
- */
 $GLOBALS['TL_LANG']['CTE']['teaser_element'] = ['Seitenteaser', 'Seitenteaser'];
 
 $GLOBALS['TL_LANG']['tl_page']['teaser_legend'] = 'Seitenteaser Text';
-$GLOBALS['TL_LANG']['tl_page']['teaser_headline'] = ['Seitenteaser Überschrift', 'Seitenteaser Überschrift'];
-$GLOBALS['TL_LANG']['tl_page']['teaser_text'] = ['Seitenteaser Text', 'Seitenteaser Text'];
+$GLOBALS['TL_LANG']['tl_page']['teaser_headline'] = ['Seitenteaser Überschrift', ''];
+$GLOBALS['TL_LANG']['tl_page']['teaser_text'] = ['Seitenteaser Text', ''];
 
-$GLOBALS['TL_LANG']['tl_page']['teaser_legend_root'] = 'Aussehen der Teaser';
-$GLOBALS['TL_LANG']['tl_page']['teaser_image_size_id'] = ['Seitenteaser Bildgröße', ''];
-$GLOBALS['TL_LANG']['tl_page']['teaser_without_text'] = ['Seitenteaser ohne Text darstellen', ''];
+$GLOBALS['TL_LANG']['tl_layout']['teaser_legend'] = 'Aussehen der Teaser';
+$GLOBALS['TL_LANG']['tl_layout']['teaser_image_size_id'] = ['Seitenteaser Bildgröße', ''];
+$GLOBALS['TL_LANG']['tl_layout']['teaser_without_text'] = ['Seitenteaser ohne Text darstellen', ''];
 
 $GLOBALS['TL_LANG']['tl_content']['teaser_legend'] = 'Seite/n, deren Teaser dargestellt werden soll/en.';
 $GLOBALS['TL_LANG']['tl_content']['teaser_pages'] = ['Seite/n für die ein Teaser-Element dargestellt werden soll.', 'Wenn nichts ausgewählt ist, werden Teaser von allen Unterseiten dargestellt. Wenn die Seite keine Unterseiten hat, werden alle anderen Seiten der gleichen Navigationsebene dargestellt.'];
@@ -31,14 +18,3 @@ $GLOBALS['TL_LANG']['tl_content']['teaser_min_width'] = ['minimale Teaser-Breite
 $GLOBALS['TL_LANG']['tl_content']['teaser_max_width'] = ['maximale Teaser-Breite', 'Standardeinstellung ist 33.33%. Eingegeben können Werte mit Einheiten. Bspw.: 100px oder 20% oder 20vw usw.'];
 $GLOBALS['TL_LANG']['tl_content']['teaser_only_sub_pages'] = ['Nicht die ausgewählte/n Seite/n verwenden, sondern alle Unterseiten.', ''];
 $GLOBALS['TL_LANG']['tl_content']['teaser_image_size_id'] = ['Bildgröße', ''];
-
-
-/**
- * Miscelaneous
- */
-//$GLOBALS['TL_LANG']['MSC'][''] = '';
-
-/**
- * Errors
- */
-//$GLOBALS['TL_LANG']['ERR'][''] = '';
