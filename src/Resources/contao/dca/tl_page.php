96653d754cfd416936d9b8f3040d306084358682
Niels <info@heimseiten.de>
2021-06-22 21:18:57 +0200

    Update tl_page.php

--- src/Resources/contao/dca/tl_page.php
+++ src/Resources/contao/dca/tl_page.php
@@ -7,7 +7,9 @@ PaletteManipulator::create()
     ->addLegend('teaser_legend', 'image_legend', PaletteManipulator::POSITION_AFTER)
     ->addField('teaser_headline','teaser_legend', PaletteManipulator::POSITION_APPEND)
     ->addField('teaser_text',    'teaser_legend', PaletteManipulator::POSITION_APPEND)
-    ->applyToPalette('regular', 'tl_page') 
+    ->applyToPalette('regular', 'tl_page')
+    ->applyToPalette('forward', 'tl_page') 
+    ->applyToPalette('redirect', 'tl_page')  
 ;
 
 $GLOBALS['TL_DCA']['tl_page']['fields'] += [    
