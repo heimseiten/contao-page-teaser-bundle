11ab6994e103c969d3a78fd48c673c0bb84c0644
Niels <info@heimseiten.de>
2022-09-15 11:55:44 +0200

    Update config.php

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,26 +1,20 @@
 <?php
-
-/*
- * This file is part of Teaser.
- * 
- * (c) Niels Hegmans 2020 <info@heimseiten.de>
- * @license GPL-3.0-or-later
- * For the full copyright and license information,
- * please view the LICENSE file that was distributed with this source code.
- * @link https://github.com/heimseiten/contao-page-teaser-bundle
- */
+use Contao\StringUtil;  
 
 function getImage($page_id, $page_pid, $pages, $size_id, $without_text, $teaser_only_sub_pages) {        
+        $db = \Contao\System::getContainer()->get('database_connection');
+        $arrResults = $db->executeQuery("SELECT id, teaser_image_size_id, teaser_without_text FROM `tl_layout` WHERE id =". $GLOBALS['objPage']->layoutId ." ;")->fetch(); 
+
         if ($pages) {
             if ($teaser_only_sub_pages) {
-                $arrResults = \Database::getInstance()->query("SELECT `id`, `pid`, `title`, `pageTitle`, `description`, `teaser_headline`, `teaser_text`, `pageImage`, `target` FROM `tl_page` WHERE pid in (". implode(',',$pages) .") AND `type`!='folder' AND `type`!='error_404' AND `hide`!='1' AND `published`='1' AND `id`!='".$page_id."' ORDER BY `sorting`;")->fetchAllAssoc();    
+                $arrResults = $db->executeQuery("SELECT `id`, `pid`, `title`, `pageTitle`, `description`, `teaser_headline`, `teaser_text`, `pageImage`, `target` FROM `tl_page` WHERE pid in (". implode(',',$pages) .") AND `type`!='folder' AND `type`!='error_404' AND `hide`!='1' AND `published`='1' AND `id`!='".$page_id."' ORDER BY `sorting`;")->fetchAll();    
             } else {
-                $arrResults = \Database::getInstance()->query("SELECT `id`, `title`, `pageTitle`, `description`, `teaser_headline`, `teaser_text`, `pageImage`, `target` FROM `tl_page` WHERE id in (". implode(',',$pages) .") AND `type`!='folder' AND `type`!='error_404' AND `published`='1' AND `id`!='".$page_id."' ORDER BY `sorting`;")->fetchAllAssoc();    
+                $arrResults = $db->executeQuery("SELECT `id`, `title`, `pageTitle`, `description`, `teaser_headline`, `teaser_text`, `pageImage`, `target` FROM `tl_page` WHERE id in (". implode(',',$pages) .") AND `type`!='folder' AND `type`!='error_404' AND `published`='1' AND `id`!='".$page_id."' ORDER BY `sorting`;")->fetchAll();    
             }
         } else {
-            $arrResults = \Database::getInstance()->query(    "SELECT `id`, `title`, `pageTitle`, `description`, `teaser_headline`, `teaser_text`, `pageImage`, `target` FROM `tl_page` WHERE pid =". $page_id ." AND `type`!='folder' AND `type`!='error_404' AND `hide`!='1' AND `published`='1' ORDER BY `sorting`;")->fetchAllAssoc();    
+            $arrResults = $db->executeQuery("SELECT `id`, `title`, `pageTitle`, `description`, `teaser_headline`, `teaser_text`, `pageImage`, `target` FROM `tl_page` WHERE pid =". $page_id ." AND `type`!='folder' AND `type`!='error_404' AND `hide`!='1' AND `published`='1' ORDER BY `sorting`;")->fetchAll();    
             if ($arrResults == NULL){ 
-                $arrResults = \Database::getInstance()->query("SELECT `id`, `pid`, `title`, `pageTitle`, `description`, `teaser_headline`, `teaser_text`, `pageImage`, `target` FROM `tl_page` WHERE pid =". $page_pid ." AND `type`!='folder' AND `type`!='error_404' AND `hide`!='1' AND `published`='1' AND `id`!='".$page_id."' ORDER BY `sorting`;")->fetchAllAssoc();    
+                $arrResults = $db->executeQuery("SELECT `id`, `pid`, `title`, `pageTitle`, `description`, `teaser_headline`, `teaser_text`, `pageImage`, `target` FROM `tl_page` WHERE pid =". $page_pid ." AND `type`!='folder' AND `type`!='error_404' AND `hide`!='1' AND `published`='1' AND `id`!='".$page_id."' ORDER BY `sorting`;")->fetchAll();    
             }            
         }
         $length = count($arrResults);
@@ -41,28 +35,28 @@ function getImage($page_id, $page_pid, $pages, $size_id, $without_text, $teaser_
                         renderPageImage( $arrResults[$i]['pageImage'], $size_id ); 
                     } else {
                         $pageId = $arrResults[$i]['id'];
-                        $tl_article = \Database::getInstance()->query("SELECT id, pid, articleImage FROM `tl_article` WHERE pid=".$pageId." ORDER BY `sorting` LIMIT 4;")->fetchAllAssoc();
+                        $tl_article = $db->executeQuery("SELECT id, pid, articleImage FROM `tl_article` WHERE pid=".$pageId." ORDER BY `sorting` LIMIT 4;")->fetchAll();
                         if ($tl_article[0]['articleImage'] !='') {
-                            echo('{{picture::'.\FilesModel::findByUuid($tl_article[0]['articleImage'])->path.'?size='.$size_id.'}}');
+                            echo('{{picture::'.\Contao\FilesModel::findByUuid($tl_article[0]['articleImage'])->path.'?size='.$size_id.'}}');
                         } else {  
                             if ($tl_article[0]['id']) {
-                                $tl_content = \Database::getInstance()->query("SELECT id, pid, invisible, singleSRC FROM `tl_content` WHERE pid=".$tl_article[0]['id']." AND `invisible`!='1' AND `singleSRC`!='' AND ((`addImage`='1') OR (`type`='image')) ORDER BY `sorting` LIMIT 5;")->fetchAllAssoc();
+                                $tl_content = $db->executeQuery("SELECT id, pid, invisible, singleSRC FROM `tl_content` WHERE pid=".$tl_article[0]['id']." AND `invisible`!='1' AND `singleSRC`!='' AND ((`addImage`='1') OR (`type`='image')) ORDER BY `sorting` LIMIT 5;")->fetchAll();
                                 if ($tl_content[0]['singleSRC']!='') { 
-                                    echo('{{picture::'.\FilesModel::findByUuid($tl_content[0]['singleSRC'])->path.'?size='.$size_id.'}}');
+                                    echo('{{picture::'.\Contao\FilesModel::findByUuid($tl_content[0]['singleSRC'])->path.'?size='.$size_id.'}}');
                                 } else {
                                     if ($tl_article[1]['id']) {
-                                        $tl_content = \Database::getInstance()->query("SELECT id, pid, invisible, singleSRC FROM `tl_content` WHERE pid=".$tl_article[1]['id']." AND `invisible`!='1' AND `singleSRC`!='' AND ((`addImage`='1') OR (`type`='image')) ORDER BY `sorting` LIMIT 5;")->fetchAllAssoc();
+                                        $tl_content = $db->executeQuery("SELECT id, pid, invisible, singleSRC FROM `tl_content` WHERE pid=".$tl_article[1]['id']." AND `invisible`!='1' AND `singleSRC`!='' AND ((`addImage`='1') OR (`type`='image')) ORDER BY `sorting` LIMIT 5;")->fetchAll();
                                         if ($tl_content[1]['singleSRC']!='') { 
-                                            echo('{{picture::'.\FilesModel::findByUuid($tl_content[0]['singleSRC'])->path.'?size='.$size_id.'}}');
+                                            echo('{{picture::'.\Contao\FilesModel::findByUuid($tl_content[0]['singleSRC'])->path.'?size='.$size_id.'}}');
                                         } else {
                                             if ($tl_article[2]['id']) {
-                                                $tl_content = \Database::getInstance()->query("SELECT id, pid, invisible, singleSRC FROM `tl_content` WHERE pid=".$tl_article[2]['id']." AND `invisible`!='1' AND `singleSRC`!='' AND ((`addImage`='1') OR (`type`='image')) ORDER BY `sorting` LIMIT 5;")->fetchAllAssoc();
+                                                $tl_content = $db->executeQuery("SELECT id, pid, invisible, singleSRC FROM `tl_content` WHERE pid=".$tl_article[2]['id']." AND `invisible`!='1' AND `singleSRC`!='' AND ((`addImage`='1') OR (`type`='image')) ORDER BY `sorting` LIMIT 5;")->fetchAll();
                                                 if ($tl_content[2]['singleSRC']!='') { 
-                                                    echo('{{picture::'.\FilesModel::findByUuid($tl_content[0]['singleSRC'])->path.'?size='.$size_id.'}}');
+                                                    echo('{{picture::'.\Contao\FilesModel::findByUuid($tl_content[0]['singleSRC'])->path.'?size='.$size_id.'}}');
                                                 } else {
                                                     if ($tl_article[3]['id']) {
-                                                        $tl_content = \Database::getInstance()->query("SELECT id, pid, invisible, singleSRC FROM `tl_content` WHERE pid=".$tl_article[3]['id']." AND `invisible`!='1' AND `singleSRC`!='' AND ((`addImage`='1') OR (`type`='image')) ORDER BY `sorting` LIMIT 5;")->fetchAllAssoc();
-                                                        echo('{{picture::'.\FilesModel::findByUuid($tl_content[0]['singleSRC'])->path.'?size='.$size_id.'}}');
+                                                        $tl_content = $db->executeQuery("SELECT id, pid, invisible, singleSRC FROM `tl_content` WHERE pid=".$tl_article[3]['id']." AND `invisible`!='1' AND `singleSRC`!='' AND ((`addImage`='1') OR (`type`='image')) ORDER BY `sorting` LIMIT 5;")->fetchAll();
+                                                        echo('{{picture::'.\Contao\FilesModel::findByUuid($tl_content[0]['singleSRC'])->path.'?size='.$size_id.'}}');
                                                     }    
                                                 }    
                                             }
@@ -85,8 +79,8 @@ function getImage($page_id, $page_pid, $pages, $size_id, $without_text, $teaser_
 }
 
 function renderPageImage($pageImage, $size_id) {
-    $multisrc = deserialize ($pageImage);
-    $objFiles = \FilesModel::findMultipleByUuids ($multisrc);
+    $multisrc = StringUtil::deserialize ($pageImage);
+    $objFiles = \Contao\FilesModel::findMultipleByUuids ($multisrc);
     if ($objFiles) {
         while ( $objFiles->next () ) {
             $image_path = $objFiles->path;
