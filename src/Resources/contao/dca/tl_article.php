1d38e78f4792b0df7a79ac03a0f2367b56486e61
Niels <info@heimseiten.de>
2022-09-15 10:02:30 +0200

    Update tl_article.php

--- src/Resources/contao/dca/tl_article.php
+++ src/Resources/contao/dca/tl_article.php
@@ -2,9 +2,10 @@
 use Contao\CoreBundle\DataContainer\PaletteManipulator;
 use Contao\DataContainer;
 
+
 $GLOBALS['TL_DCA']['tl_article']['fields']['articleImage'] = [ 
     'label'     => &$GLOBALS['TL_LANG']['tl_article']['articleImage'],
     'inputType' => 'fileTree',
-    'eval' => array('tl_class'  => 'w50', 'fieldType' => 'radio', 'filesOnly' => true, 'extensions' => \Config::get('validImageTypes') ),
+    'eval' => array('tl_class'  => 'w50', 'fieldType' => 'radio', 'filesOnly' => true ),
     'sql'       => "blob NULL"
 ];
