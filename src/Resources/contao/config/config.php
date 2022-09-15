3a975d3af54e4fb9d2867411fa4d02ef05b4ac7d
Niels <info@heimseiten.de>
2022-09-15 09:40:00 +0200

    Update config.php

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -10,8 +10,7 @@
  * @link https://github.com/heimseiten/contao-page-teaser-bundle
  */
 
-function getImage($page_id, $page_pid, $pages, $size_id, $without_text, $teaser_only_sub_pages) {    
-    if (TL_MODE == "FE") {
+function getImage($page_id, $page_pid, $pages, $size_id, $without_text, $teaser_only_sub_pages) {        
         if ($pages) {
             if ($teaser_only_sub_pages) {
                 $arrResults = \Database::getInstance()->query("SELECT `id`, `pid`, `title`, `pageTitle`, `description`, `teaser_headline`, `teaser_text`, `pageImage`, `target` FROM `tl_page` WHERE pid in (". implode(',',$pages) .") AND `type`!='folder' AND `type`!='error_404' AND `hide`!='1' AND `published`='1' AND `id`!='".$page_id."' ORDER BY `sorting`;")->fetchAllAssoc();    
@@ -83,7 +82,6 @@ function getImage($page_id, $page_pid, $pages, $size_id, $without_text, $teaser_
                 echo '</a></div>';
             echo '</div>';
         }
-    }
 }
 
 function renderPageImage($pageImage, $size_id) {
