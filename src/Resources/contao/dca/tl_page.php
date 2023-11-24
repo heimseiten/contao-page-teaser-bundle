2d60b29547eba0e1c9725f8408e30857b10b4f36
Niels <info@heimseiten.de>
2023-11-24 11:24:56 +0100

    Update tl_page.php

--- src/Resources/contao/dca/tl_page.php
+++ src/Resources/contao/dca/tl_page.php
@@ -5,7 +5,6 @@ use Contao\CoreBundle\DataContainer\PaletteManipulator;
 
 PaletteManipulator::create()
     ->addLegend('teaser_legend', 'title_legend', PaletteManipulator::POSITION_AFTER)
-    ->addField('pageImage','teaser_legend', PaletteManipulator::POSITION_APPEND)
     ->addField('teaser_headline','teaser_legend', PaletteManipulator::POSITION_APPEND)
     ->addField('teaser_text',    'teaser_legend', PaletteManipulator::POSITION_APPEND)
     ->applyToPalette('regular', 'tl_page')
@@ -14,13 +13,6 @@ PaletteManipulator::create()
 ;
 
 $GLOBALS['TL_DCA']['tl_page']['fields'] += [    
-    'pageImage' => [
-        'label'         => &$GLOBALS['TL_LANG']['tl_page']['pageImage'],
-        'inputType'     => 'fileTree',
-        'exclude'       => true,
-        'eval'          => array('fieldType'=>'checkbox', 'orderField'=>'pageImageOrder', 'multiple'=>true, 'files'=>true, 'filesOnly'=>true, 'extensions'=> Config::get('validImageTypes'), 'isGallery'=>true),
-        'sql'           => 'blob NULL'
-    ],
     'pageImageOrder' => [
         'eval'          => array('doNotShow'=>true),
         'sql'           => 'blob NULL'
