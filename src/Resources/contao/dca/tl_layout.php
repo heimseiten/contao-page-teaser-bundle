e67ccdc6f2439f6ba970653ea1a1774bf536393c
Niels <info@heimseiten.de>
2026-06-18 12:06:48 +0200

    Backend: Teaser-Felder nebeneinander (tl_class w50)
    Seiten-Einstellungen: teaser_headline + teaser_text nebeneinander.
    Seitenlayout: teaser_without_text neben teaser_image_size_id.

--- src/Resources/contao/dca/tl_layout.php
+++ src/Resources/contao/dca/tl_layout.php
@@ -1,6 +1,5 @@
 <?php
 
-use Contao\Config;
 use Contao\CoreBundle\DataContainer\PaletteManipulator;
 
 // Add the teaser settings under the existing image sizes legend ("Bildgrößen"),
@@ -11,20 +10,20 @@ PaletteManipulator::create()
     ->applyToPalette('modern', 'tl_layout')
 ;
 
-$GLOBALS['TL_DCA']['tl_layout']['fields'] += [    
+$GLOBALS['TL_DCA']['tl_layout']['fields'] += [
     'teaser_image_size_id' => [
-            'exclude'                 => true,
-			'inputType'               => 'imageSize',
-			'reference'               => &$GLOBALS['TL_LANG']['MSC'],
-			'eval'                    => array('rgxp'=>'natural', 'includeBlankOption'=>true, 'nospace'=>true, 'helpwizard'=>true, 'tl_class'=>'w50'),
-			'options_callback' => static function ()
-			{
-				return Contao\System::getContainer()->get('contao.image.sizes')->getOptionsForUser(Contao\BackendUser::getInstance());
-			},
-			'sql'                     => "varchar(255) NOT NULL default ''"
+        'exclude'          => true,
+        'inputType'        => 'imageSize',
+        'reference'        => &$GLOBALS['TL_LANG']['MSC'],
+        'eval'             => ['rgxp' => 'natural', 'includeBlankOption' => true, 'nospace' => true, 'helpwizard' => true, 'tl_class' => 'w50'],
+        'options_callback' => static function () {
+            return Contao\System::getContainer()->get('contao.image.sizes')->getOptionsForUser(Contao\BackendUser::getInstance());
+        },
+        'sql'              => "varchar(255) NOT NULL default ''",
     ],
     'teaser_without_text' => [
-        'inputType'     => 'checkbox', 
-        'sql'           => "char(1) NOT NULL default ''"
+        'inputType' => 'checkbox',
+        'eval'      => ['tl_class' => 'w50'],
+        'sql'       => "char(1) NOT NULL default ''",
     ],
 ];
--- src/Resources/contao/dca/tl_page.php
+++ src/Resources/contao/dca/tl_page.php
@@ -1,26 +1,27 @@
 <?php
 
-use Contao\Config;
 use Contao\CoreBundle\DataContainer\PaletteManipulator;
 
 PaletteManipulator::create()
     ->addLegend('teaser_legend', 'title_legend', PaletteManipulator::POSITION_AFTER)
-    ->addField('teaser_headline','teaser_legend', PaletteManipulator::POSITION_APPEND)
-    ->addField('teaser_text',    'teaser_legend', PaletteManipulator::POSITION_APPEND)
+    ->addField('teaser_headline', 'teaser_legend', PaletteManipulator::POSITION_APPEND)
+    ->addField('teaser_text', 'teaser_legend', PaletteManipulator::POSITION_APPEND)
     ->applyToPalette('regular', 'tl_page')
-    ->applyToPalette('forward', 'tl_page') 
-    ->applyToPalette('redirect', 'tl_page')  
+    ->applyToPalette('forward', 'tl_page')
+    ->applyToPalette('redirect', 'tl_page')
 ;
 
-$GLOBALS['TL_DCA']['tl_page']['fields'] += [    
+$GLOBALS['TL_DCA']['tl_page']['fields'] += [
     'teaser_headline' => [
-        'label'     => &$GLOBALS['TL_LANG']['tl_page']['teaser_headline'], 
-        'inputType' => 'text', 
-        'sql'       => "text NULL" 
+        'label'     => &$GLOBALS['TL_LANG']['tl_page']['teaser_headline'],
+        'inputType' => 'text',
+        'eval'      => ['tl_class' => 'w50'],
+        'sql'       => "text NULL",
     ],
     'teaser_text' => [
         'label'     => &$GLOBALS['TL_LANG']['tl_page']['teaser_text'],
         'inputType' => 'textarea',
-        'sql'       => "text NULL"
+        'eval'      => ['tl_class' => 'w50'],
+        'sql'       => "text NULL",
     ],
 ];
