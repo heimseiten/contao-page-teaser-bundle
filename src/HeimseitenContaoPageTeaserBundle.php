d48ce50b2bcad575ce249f7d50e79c6cad7470f8
Niels <info@heimseiten.de>
2026-06-23 11:29:11 +0200

    Modernize bundle layout (AbstractBundle, resources at root); remove boilerplate tests

--- src/HeimseitenContaoPageTeaserBundle.php
+++ src/HeimseitenContaoPageTeaserBundle.php
@@ -2,23 +2,16 @@
 
 declare(strict_types=1);
 
-/*
- * This file is part of Teaser.
- * 
- * (c) Niels Hegmans 2020 <info@heimseiten.de>
- * @license GPL-3.0-or-later
- * For the full copyright and license information,
- * please view the LICENSE file that was distributed with this source code.
- * @link https://github.com/heimseiten/contao-page-teaser-bundle
- */
-
 namespace Heimseiten\ContaoPageTeaserBundle;
 
-use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
-/**
- * Class HeimseitenContaoPageTeaserBundle
- */
-class HeimseitenContaoPageTeaserBundle extends Bundle
+class HeimseitenContaoPageTeaserBundle extends AbstractBundle
 {
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $container->import('../config/services.yml');
+    }
 }
